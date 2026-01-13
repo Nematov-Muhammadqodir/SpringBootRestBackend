157ea96e932209028dea9677be76dffe1a8938be
ya.nematov <nematovmuhammadqodir68@gmail.com>
2026-01-14 00:38:42 +0900

    initailize the app and build getAllJobs

---
+++ src/main/java/com/kevin/spring_boot_rest/JobRestController.java
@@ -0,0 +1,23 @@
+package com.kevin.spring_boot_rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kevin.spring_boot_rest.model.JobPost;
+import com.kevin.spring_boot_rest.service.JobService;
+
+@RestController
+public class JobRestController {
+
+    @Autowired
+    private JobService service;
+
+    @GetMapping("jobPosts")
+    public List<JobPost> getAllJobs() {
+        return service.getAllJobs();
+    }
+
+}
